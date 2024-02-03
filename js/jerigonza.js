051f995ce86ee3f1fe7a5225cc7845fbe6b657da
olaZelys <pace.celis@gmail.com>
2024-02-02 22:48:02 -0300

    update

--- js/jerigonza.js
+++ js/jerigonza.js
@@ -1,79 +1,78 @@
 // Capturar elementos HTML
 
-const entrada = document.querySelector(".entrada-texto");
-
+const entrada = document.getElementById("entrada-texto");
+const avisoEntrada = document.querySelector(".aviso-texto");
 const btnEncriptar = document.querySelector(".btn-encriptar");
-
 const btnDesencriptar = document.querySelector(".btn-desencriptar");
-
-const salida = document.querySelector(".salida-texto");
-
-const aviso = document.querySelector(".salida-aviso");
-
+const salida = document.getElementById("salida-texto");
+const avisoSalida = document.querySelector(".salida-aviso");
 const btnCopiar = document.querySelector(".btn-copiar");
 
-// Función encriptar (primera forma)
+// Funciones encriptar y desencriptar
 
-function convertirAJerigonza(texto) {
-  const reemplazos = {
-    a: "apa",
-    e: "epe",
-    i: "ipi",
-    o: "opo",
-    u: "upu",
-  };
-
-  return texto.replace(/a|e|i|o|u/g, (match) => reemplazos[match]);
-}
-
-// Función desencriptar (segunda forma)
-
-function revertirJerigonza(texto) {
-  const reemplazos = {
-    apa: "a",
-    epe: "e",
-    ipi: "i",
-    opo: "o",
-    upu: "u",
-  };
-
-  return texto.replace(/apa|epe|ipi|opo|upu/g, (match) => reemplazos[match]);
-}
-
-// Función para quitar acentos
-
-function quitarAcentos(textoConAcentos) {
-  return textoConAcentos.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-}
-
-// Capturar evento del boton encriptar
-
-btnEncriptar.addEventListener("click", (event) => {
-  let texto = entrada.value;
-
-  salida.innerHTML = convertirAJerigonza(quitarAcentos(texto.toLowerCase()));
-
-  aviso.remove();
-
+function encriptar(texto) {
+  salida.value = texto
+    .replaceAll("e", "epe")
+    .replaceAll("i", "ipi")
+    .replaceAll("a", "apa")
+    .replaceAll("o", "opo")
+    .replaceAll("u", "upu");
+  avisoSalida.remove();
   btnCopiar.style.visibility = "inherit";
+}
+
+function desencriptar(texto) {
+  salida.value = texto
+    .replaceAll("epe", "e")
+    .replaceAll("ipi", "i")
+    .replaceAll("apa", "a")
+    .replaceAll("opo", "o")
+    .replaceAll("upu", "u");
+  avisoSalida.remove();
+  btnCopiar.style.visibility = "inherit";
+}
+
+// Aviso de validación
+
+function textoNoValido() {
+  avisoEntrada.style.color = "#FF0000";
+  avisoEntrada.style.fontWeight = "700";
+  avisoEntrada.textContent = "Ingresa un texto valido.";
+  setTimeout(() => {
+    avisoEntrada.removeAttribute("style");
+    avisoEntrada.textContent =
+      "No ingresar mayúsculas, caracteres especiales y acentos.";
+  }, 2500);
+}
+
+// Validar texto de entrada
+
+function validarTexto(entradaTxt) {
+  // Verificar si la entrada está vacía
+  if (entradaTxt.trim() === "") {
+    return true;
+  }
+  // Retorna true solamente si la expresión regular encuentra coincidencias en la cadena.
+  return /[A-ZÁÉÍÓÚÜ]|[^A-Za-z0-9\sñ]/.test(entradaTxt);
+}
+
+// Capturar entradas de texto
+
+btnEncriptar.addEventListener("click", () => {
+  let txt = entrada.value;
+
+  validarTexto(txt) ? textoNoValido() : encriptar(txt);
 });
 
-// Capturar evento del boton desencriptar
+btnDesencriptar.addEventListener("click", () => {
+  let txt = entrada.value;
 
-btnDesencriptar.addEventListener("click", (event) => {
-  let texto = entrada.value;
-
-  salida.innerHTML = revertirJerigonza(quitarAcentos(texto.toLowerCase()));
-
-  aviso.remove();
-
-  btnCopiar.style.visibility = "inherit";
+  validarTexto(txt) ? textoNoValido() : desencriptar(txt);
 });
 
-// Boton copiar
+// Copiar texto de salida
 
-btnCopiar.addEventListener("click", (event) => {
+btnCopiar.addEventListener("click", () => {
   salida.select();
-
   document.execCommand("copy");
 });
